9d09bd9e970445ca502bac1d809e376303fe0fbe
hadzikk <hadzikmochammad@gmail.com>
2024-01-10 20:27:15 +0700

    menambahkan dashboard_main.php (halaman utama pada dashboard)

---
+++ dashboard_main.php
@@ -0,0 +1,54 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard Main</title>
+    <link rel="stylesheet" href="css/dashboard.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="shortcut icon" href="https://cdn-icons-png.flaticon.com/128/1828/1828791.png?ga=GA1.1.1095638270.1704336278&semt=ais" type="image/x-icon">
+</head>
+<body>
+    <section class="sidebar">
+        <header><p>perpustakaan digital</p></header>
+
+        <div class="admin-tools">
+            <p>admin tools</p>
+            <ul>
+                <li class="active"><i class="fa-solid fa-table-columns"></i> &nbsp; <a href="dashboard_main.php">main</a></li>
+                <li><i class="fa-solid fa-book"></i> &nbsp; <a href="dashboard_data_buku.php">buku</a></li>
+                <li><i class="fa-regular fa-user"></i> &nbsp;<a href="">pengguna</a></li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="container">
+        <nav>
+            <div class="text">
+                <p>Hi Hadzik</p>
+                <p>Schedule</p>
+            </div>
+            <div class="nav-features">
+                <div class="search-box">
+                    <form action="">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                        <input type="text" placeholder="cari">
+                    </form>
+                </div>
+                <div class="notification-box">
+                    <i class="fa-regular fa-bell"></i>
+                </div>
+            </div>
+        </nav>
+
+        <div class="banner">
+            <div class="banner-text">
+                <h2>main dashboard</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis, doloribus?</p>
+                <div class="banner-button">
+                    <a href="">New button</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
